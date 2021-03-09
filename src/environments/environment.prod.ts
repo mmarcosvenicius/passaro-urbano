ab42339351727038e54d024ba2bca98c1e000bb7
marcos <marcos.venicius@morphy.com.br>
2021-03-09 16:34:51 -0300

    modified urlApi prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  urlApi: 'https://my-json-server.typicode.com/mmarcosvenicius/json-database'
+  urlApi: 'https://my-json-server.typicode.com/mmarcosvenicius/passaro-urbano-bd'
 };
