0b3d7e614a41e538f8cdc152ee63e092b7da72cc
Marcos Venicius <41882652+mmarcosvenicius@users.noreply.github.com>
2022-10-28 14:58:49 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  urlApi: 'https://my-json-server.typicode.com/mmarcosvenicius/passaro-urbano-bd'
+  urlApi: 'https://my-json-server.typicode.com/mmarcosvenicius/bd'
 };
